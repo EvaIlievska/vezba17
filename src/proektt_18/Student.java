108a83ea8b67a56f139d1827dce02cf43f3da041
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 09:14:51 +0100

    vezba17

---
+++ src/proektt_18/Student.java
@@ -0,0 +1,11 @@
+package proektt_18;
+
+//A Java class which has only getter methods.  
+	public class Student{  
+	//private data member  
+	private String college="AKG";  
+	//getter method for college  
+	public String getCollege(){  
+	return college;  
+	}  
+}
---
+++ src/proektt_18/Studentt.java
@@ -0,0 +1,9 @@
+package proektt_18;
+
+//A Java class which has only setter methods.  
+public class Studentt {  
+	//getter method for college  
+	public void setCollege(String college){  
+	
+	}  
+	}
